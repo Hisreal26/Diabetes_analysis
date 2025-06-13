aa98a622c379f3d8dc9a6d28ae9fd7e320ba2fc8
AKINBAMI OLAOLUWA ISRAEL <oakinbamiisreal@gmail.com>
2025-06-13 13:54:04 +0100

    Create Diabetes_analysis.py

---
+++ Diabetes_analysis.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as pt
+import numpy as np
+import seaborn as sns
+
+#read a csv file
+#import csv file
+#convert file to dataframe
+
+df = pd.read_csv["Diabetes.csv"]
+st.write(df.head())
+
+
+
+
+
+
+#import  python -m pip install scikit-learn
+#import -m pip install matlib
+#import -m pip install seaborn

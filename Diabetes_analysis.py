ac381f514227aca91df91a3c8671c4fa524ad534
AKINBAMI OLAOLUWA ISRAEL <oakinbamiisreal@gmail.com>
2025-06-16 12:36:09 +0100

    Update Diabetes_analysis.py

--- Diabetes_analysis.py
+++ Diabetes_analysis.py
@@ -3,17 +3,28 @@ import streamlit as st
 import matplotlib.pyplot as pt
 import numpy as np
 import seaborn as sns
+import plotly.express as px
 
 #read a csv file
 #import csv file
 #convert file to dataframe
 
+
+st.title("Diabetes.csv")
+#import my csv file
 df = pd.read_csv("Diabetes.csv")
 st.write(df.head())
 
 df = pd.read_csv("Diabetes.csv")
 st.write(df.tail())
 
+
+numpy==2.2.6
+matplotlib==3.10.3
+pandas==2.2.3
+scikit-learn==1.7.0
+seaborn==0.13.2
+streamlit==1.45.1
 
 
 

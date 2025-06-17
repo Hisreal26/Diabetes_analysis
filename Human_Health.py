e0eafeed9807e1a775e61978834a402bef8c3efc
AKINBAMI OLAOLUWA ISRAEL <oakinbamiisreal@gmail.com>
2025-06-17 19:02:13 +0100

    Update Human_Health.py

--- Human_Health.py
+++ Human_Health.py
@@ -9,17 +9,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 
-st.title("# DIABETES ANALYSIS")
+st.title("DIABETES ANALYSIS")
 st.markdown("## OVERVIEW")
 
 #import my csv file
-st.markdown("### FIRST SEVEN OBSERVATIONS")
+st.markdown("### FIRST FIVE OBSERVATIONS")
 df = pd.read_csv("Diabetes.csv")
-st.write(df.head(7))
+st.write(df.head())
 
-st.markdown("### LAST SEVEN OBSERVATIONS")
+st.markdown("### LAST FIVE OBSERVATIONS")
 df = pd.read_csv("Diabetes.csv")
-st.write(df.tail(7))
+st.write(df.tail())
 
 st.markdown("### DATA INFO")
 AK = df.shape
@@ -32,8 +32,8 @@ st.write(correlation)
 st.markdown("### BLOOD PRESSURE")
 st.write(df["BloodPressure"].describe())
 
-st.markdown("### FIRST SEVEN BLOOD PRESSURE")
-st.write(df["BloodPressure"].head(7))
+st.markdown("### FIRST FIVE BLOOD PRESSURE")
+st.write(df["BloodPressure"].head())
 
 #UNIVARIATE ANALYSIS
 st.markdown("### UNIVARIATE ANALYSIS")
@@ -61,14 +61,27 @@ st.markdown("### INSULIN ANALYSIS")
 df = pd.read_csv("Diabetes.csv")
 st.write(df["Insulin"].describe())
 
-st.title("HISTOGRAM REPRESENTATION")
+st.markdown("### HISTOGRAM REPRESENTATION FOR BP")
 BP = px.histogram(df["BloodPressure"], y= "BloodPressure", title="Pressure Distribution")
 st.plotly_chart(BP, use_container_width=True)
 
-st.title("BAR REPRESENTATION")
+st.markdown("### LINE GRAPH REPRESENTATION FOR BP")
+BP = px.line(df["BloodPressure"], y= "BloodPressure", title="Pressure Distribution")
+st.plotly_chart(BP, use_container_width=True)
+
+st.markdown("### BAR REPRESENTATION FOR BP")
 BP2 = px.bar(df["BloodPressure"], y= "BloodPressure", title="Pressure Distribution")
 st.plotly_chart(BP2, use_container_width=True)
 
+st.markdown("### HISTOGRAM REPRESENTATION FOR PREGNANCIES")
+Pregg = px.histogram(df["Pregnancies"], y ="Pregnancies", title = "Pregnancies Distribution")
+st.plotly_chart(Pregg, use_container_width = True)
+
+st.markdown("### LINE GRAPH REPRESENTATION FOR PREGNANCIES")
+Pregg = px.line(df["Pregnancies"], y ="Pregnancies", title = "Pregnancies Distribution")
+st.plotly_chart(Pregg, use_container_width = True)
+
+st.markdown("### BAR REPRESENTATION FOR PREGNANCIES")
 Pregg = px.bar(df["Pregnancies"], y ="Pregnancies", title = "Pregnancies Distribution")
 st.plotly_chart(Pregg, use_container_width = True)
 
@@ -78,3 +91,43 @@ st.markdown("### Blood Pressure vs Pregnancies")
 df2 = pd.DataFrame(df["BloodPressure"],df["Pregnancies"])
 st.write(df2)
 
+st.markdown("### Blood Pressure vs BMI")
+df3 = pd.DataFrame(df["BloodPressure"],df["BMI"])
+st.write(df3)
+
+st.markdown("### Glucose vs Pregnancies")
+df4 = pd.DataFrame(df["Glucose"],df["Pregnancies"])
+st.write(df4)
+
+st.markdown("### Skin Thickness vs Pregnancies")
+df5 = pd.DataFrame(df["SkinThickness"],df["Pregnancies"])
+st.write(df5)
+
+st.markdown("### Age vs Pregnancies")
+df6 = pd.DataFrame(df["Age"],df["Pregnancies"])
+st.write(df6)
+
+st.markdown("### Pregnancies vs Insulin")
+df_ = pd.DataFrame(df["Pregnancies"],df["Insulin"])
+st.write(df_)
+
+st.markdown("# PREDICTIVE ANALYSIS")
+X = df.drop("Outcome", axis=1)
+Y = df["Outcome"]
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2)
+
+model = LogisticRegression()
+model.fit(X_train,Y_train) #training the model
+
+st.markdown("## Outcome Prediction")
+prediction = model.predict(X_test)
+st.write(prediction)
+
+st.markdown("## Model Evaluation")
+accuracy = accuracy_score(prediction, Y_test)
+st.write(accuracy)
+
+
+#download by typing "python -m pip install scikit-learn"
+#download by typing "python -m pip install matlib"
+#download by typing "python -m pip install seaborn"
